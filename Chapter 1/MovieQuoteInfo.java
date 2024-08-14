3060f3378d953e91b51c93cfe07f8f343f8a5085
kirbyNicole <kirbynicolemontecillo@gmail.com>
2024-08-14 20:41:16 +0800

    Exercise A

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MovieQuoteInfo
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Maybe none of us can say who we truly are.");
+        System.out.println("Hannah Baker | Thirteen Reasons Why, 2017");
+    }
+}
