f7aef69048d0eb8b2c48117df4c69fa3d417b320
Kirby Montecillo <kirbynicolemontecillo@gmail.com>
2024-09-30 22:33:01 +0800

    Update Billing.java

--- Chapter 4/Billing.java
+++ Chapter 4/Billing.java
@@ -1,29 +1,28 @@
 class Billing
 {
-    public void computeBill(double price) {
-
+    public void computeBill(double price) 
+    {
         double total = price * (1 + 0.08);
         System.out.println("The total price for the bill is: " + total);
     }
 
-    public void computeBill(double price, int quantity) {
-
+    public void computeBill(double price, int quantity) 
+    {
         double total = (price * quantity) * (1 + 0.08);
         System.out.println("The total price for the bill is: " + total);
     }
 
 
-    public void computeBill(double price, int quantity, int discount) {
+    public void computeBill(double price, int quantity, int discount) 
+    {
         double tax = 0.08;
         double totalBeforeTax = (price * quantity) - discount;
         double total = totalBeforeTax * (1 + 0.08);
         System.out.println("The total price for the bill is: " + total);
     }
-
-
+    
     public static void main(String[] args) {
         Billing b = new Billing();
-
 
         b.computeBill(23.2);      
         b.computeBill(16.266644, 3);    
