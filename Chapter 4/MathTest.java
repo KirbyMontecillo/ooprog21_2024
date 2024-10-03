d03bc70099e29c2d1dd89fa812d46fb5ece3079e
Kirby Montecillo <kirbynicolemontecillo@gmail.com>
2024-10-03 11:01:32 +0800

    Updated MathTest.java

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,16 @@
+
+class MathTest
+{
+   public static void main (String [] args)
+   {
+      System.out.println("SQRT: " + Math.sqrt(37));
+      System.out.println("SIN: " + Math.sin(5));
+      System.out.println("COS: " + Math.cos(13));
+      System.out.println("FLOOR: " + Math.floor(8));
+      System.out.println("CEIL: " + Math.ceil(6));
+      System.out.println("ROUND: " + Math.cos(4));
+      System.out.println("MAX: " + Math.cos(1,9));
+      System.out.println("MIN: " + Math.cos(4,7));
+      System.out.println("RANDOM: " + Math.random());
+   }
+}
