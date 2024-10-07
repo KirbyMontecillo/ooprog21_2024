3484e1e44f7c48f875c0bfb130f856ca4cabb100
Kirby Montecillo <kirbynicolemontecillo@gmail.com>
2024-10-07 08:01:00 +0800

    Updated MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -8,9 +8,9 @@ class MathTest
       System.out.println("COS: " + Math.cos(13));
       System.out.println("FLOOR: " + Math.floor(8));
       System.out.println("CEIL: " + Math.ceil(6));
-      System.out.println("ROUND: " + Math.cos(4));
-      System.out.println("MAX: " + Math.cos(1,9));
-      System.out.println("MIN: " + Math.cos(4,7));
+      System.out.println("ROUND: " + Math.round(4));
+      System.out.println("MAX: " + Math.max(1,9));
+      System.out.println("MIN: " + Math.min(4,7));
       System.out.println("RANDOM: " + Math.random());
    }
 }
